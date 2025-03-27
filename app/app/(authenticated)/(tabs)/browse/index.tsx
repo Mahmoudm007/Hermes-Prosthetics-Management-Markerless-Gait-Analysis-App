c13ca1e0b2273a1923734e6bc098417317fee9cf
Bodykudo <abdallah.magdy.sadek@gmail.com>
2025-03-28 00:11:57 +0200

    Implement sign out functionality

--- app/app/(authenticated)/(tabs)/browse/index.tsx
+++ app/app/(authenticated)/(tabs)/browse/index.tsx
@@ -1,11 +1,47 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { useAuth } from '@clerk/clerk-react';
+
+import { Colors } from '@/constants/Colors';
 
 export default function BrowsePage() {
+  const { signOut } = useAuth();
+
+  const handleSignOut = () => {
+    Alert.alert('Confirm Sign Out', 'Are you sure you want to sign out?', [
+      { text: 'Cancel', style: 'cancel' },
+      { text: 'Sign Out', onPress: () => signOut(), style: 'destructive' },
+    ]);
+  };
+
   return (
-    <View>
-      <Text>BrowsePage</Text>
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.button} onPress={handleSignOut}>
+        <Text style={styles.buttonText}>Sign Out</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: Colors.primary,
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    borderRadius: 8,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
