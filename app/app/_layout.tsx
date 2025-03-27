0414406c71813d5e33aa7ed7cb748feaf1f01bc6
Bodykudo <abdallah.magdy.sadek@gmail.com>
2025-03-28 00:03:27 +0200

    Implement mobile app authentication using Clerk

--- app/app/_layout.tsx
+++ app/app/_layout.tsx
@@ -1,5 +1,75 @@
-import { Stack } from "expo-router";
+import { useEffect } from 'react';
+import { ActivityIndicator, View } from 'react-native';
+import { Stack, usePathname, useRouter, useSegments } from 'expo-router';
+import { ClerkLoaded, ClerkProvider, useAuth } from '@clerk/clerk-expo';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Toaster } from 'sonner-native';
+
+import { tokenCache } from '@/lib/cache';
+import { Colors } from '@/constants/Colors';
+
+const queryClient = new QueryClient();
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
+if (!publishableKey) {
+  throw new Error(
+    'Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env'
+  );
+}
+
+function InitialLayout() {
+  const router = useRouter();
+  const { isLoaded, isSignedIn } = useAuth();
+  const segments = useSegments();
+  const pathName = usePathname();
+
+  useEffect(() => {
+    if (!isLoaded) return;
+
+    const inAuthGroup = segments[0] === '(authenticated)';
+
+    if (isSignedIn && !inAuthGroup) {
+      router.replace('/home');
+    } else if (!isSignedIn && pathName !== '/') {
+      router.replace('/');
+    }
+  }, [isLoaded, isSignedIn, pathName, segments]);
+
+  if (!isLoaded) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size='large' color={Colors.primary} />
+      </View>
+    );
+  }
+
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+        contentStyle: {
+          backgroundColor: Colors.background,
+        },
+      }}
+    >
+      <Stack.Screen name='index' />
+    </Stack>
+  );
+}
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+      <ClerkLoaded>
+        <QueryClientProvider client={queryClient}>
+          <GestureHandlerRootView style={{ flex: 1 }}>
+            <Toaster />
+            <InitialLayout />
+          </GestureHandlerRootView>
+        </QueryClientProvider>
+      </ClerkLoaded>
+    </ClerkProvider>
+  );
 }
--- app/app/index.tsx
+++ app/app/index.tsx
@@ -1,15 +1,146 @@
-import { Text, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import * as WebBrowser from 'expo-web-browser';
+import { useSSO } from '@clerk/clerk-expo';
+import { Ionicons } from '@expo/vector-icons';
+
+import { Colors } from '@/constants/Colors';
 
 export default function Index() {
+  const { startSSOFlow } = useSSO();
+  const { top } = useSafeAreaInsets();
+
+  const handleAppleOAuth = async () => {
+    try {
+      const { createdSessionId, setActive } = await startSSOFlow({
+        strategy: 'oauth_apple',
+      });
+
+      if (createdSessionId && setActive) {
+        setActive({
+          session: createdSessionId,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleGoogleOAuth = async () => {
+    try {
+      const { createdSessionId, setActive } = await startSSOFlow({
+        strategy: 'oauth_google',
+      });
+
+      if (createdSessionId && setActive) {
+        setActive({
+          session: createdSessionId,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const openLink = async (url: string) => {
+    await WebBrowser.openBrowserAsync(url);
+  };
+
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={[
+        styles.container,
+        {
+          paddingTop: top,
+        },
+      ]}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Image
+        source={require('@/assets/images/icon.png')}
+        style={styles.loginImage}
+      />
+      <Image
+        source={require('@/assets/images/favicon.png')}
+        style={styles.bannerImage}
+      />
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button} onPress={handleAppleOAuth}>
+          <Ionicons name='logo-apple' size={24} />
+          <Text style={styles.buttonText}>Continue with Apple</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={handleGoogleOAuth}>
+          <Ionicons name='logo-google' size={24} />
+          <Text style={styles.buttonText}>Continue with Google</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Ionicons name='mail' size={24} />
+          <Text style={styles.buttonText}>Continue with Email</Text>
+        </TouchableOpacity>
+
+        <Text style={styles.description}>
+          By continuing you agree to Todoist's{' '}
+          <Text
+            style={styles.link}
+            onPress={() => openLink('https://github.com/')}
+          >
+            Terms of Service
+          </Text>{' '}
+          and{' '}
+          <Text
+            style={styles.link}
+            onPress={() => openLink('https://github.com/')}
+          >
+            Privacy Policy
+          </Text>
+          .
+        </Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    gap: 40,
+    marginTop: 20,
+  },
+  loginImage: {
+    height: 40,
+    resizeMode: 'contain',
+    alignSelf: 'center',
+  },
+  bannerImage: {
+    height: 280,
+    resizeMode: 'contain',
+    alignSelf: 'center',
+  },
+  buttonContainer: {
+    gap: 20,
+    marginHorizontal: 40,
+  },
+  button: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 10,
+    padding: 12,
+    borderRadius: 6,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: Colors.lightBorder,
+  },
+  buttonText: {
+    fontSize: 20,
+    fontWeight: '500',
+  },
+  description: {
+    fontSize: 12,
+    textAlign: 'center',
+    color: Colors.lightText,
+  },
+  link: {
+    color: Colors.lightText,
+    fontSize: 12,
+    textAlign: 'center',
+    textDecorationLine: 'underline',
+  },
+});
